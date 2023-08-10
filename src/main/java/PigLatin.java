6e46b5506b79a7183d491b3e2a7ee1b29a8b69db
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2023-08-10 22:05:36 +0530

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,11 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
+/*
+        String newString = in.substring(1);
+        newString = newString +in.substring(0,1)+"ay";
+        return newString;
+        */
         int flag=0;
         String piglatin="";
         for(int i=0;i<in.length();i++)
